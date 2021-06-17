e444625796117e198829c220d7673d6b95cc51f8
gardenia <455419694@qq.com>
2021-06-17 19:52:40 +0800

    leetcode326

--- 326.power-of-three.js
+++ 326.power-of-three.js
@@ -9,21 +9,25 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfThree = function (n) {
-  if (n == 0) {
+
+//循环方法，如果 n % 3 == 0 就继续除 3
+/*var isPowerOfThree = function (n) {
+  if (n < 1) {
     return false
   }
-  while (1) {
-    if (n == 1) {
-      break
-    }
+  while (n != 1) {
     let mol = n % 3
     if (mol != 0) {
       return false
     }
     n /= 3
-
   }
   return true
-};
+};*/
+
+//如果一个数是3^19的因子，那么它一定是3的幂
+var isPowerOfThree = function (n) {
+  return n > 0 && 1162261467 % n == 0
+}
+
 // @lc code=end
