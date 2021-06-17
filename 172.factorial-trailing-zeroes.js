9429f7ce889c8929d1c00d4c78df2e1b37bc81dd
gardenia <455419694@qq.com>
2021-06-17 21:07:09 +0800

    leedcode 172

---
+++ 172.factorial-trailing-zeroes.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=172 lang=javascript
+ *
+ * [172] Factorial Trailing Zeroes
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+//常规算法,时间复杂度高
+/*var trailingZeroes = function (n) {
+  let m = BigInt(1)
+  let ans = 0
+  for (i = 1; i <= n; i++) {
+    m *= BigInt(i)
+  }
+  while (m % BigInt(10) == 0) {
+    m /= BigInt(10)
+    ans++
+  }
+  return ans
+};*/
+
+//进阶算法 计算包含的5的倍数的数量
+var trailingZeroes = function (n) {
+  n = n - n % 5
+  let zeros = 0
+  while (n != 0) {
+    n = Math.trunc(n / 5)
+    zeros += n
+  }
+  return zeros
+}
+
+// @lc code=end
+
+// for (let i = 1; i <= 50; i++) {
+//   console.log(trailingZeroes(i), i);
+// }
