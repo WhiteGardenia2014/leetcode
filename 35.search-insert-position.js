e67059ac13a2ac34666960f55082ad3c90ae29cc
gardenia <455419694@qq.com>
2021-06-20 13:08:21 +0800

    leetcode 35

---
+++ 35.search-insert-position.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=35 lang=javascript
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let left = 0
+  let right = nums.length - 1
+  while (left <= right) {
+    let middle = Math.trunc((left + right) / 2)
+    if (target > nums[middle]) {
+      left = middle + 1
+    } else if (target < nums[middle]) {
+      right = middle - 1
+    } else {
+      return middle
+    }
+  }
+  return left
+};
+// @lc code=end
+
+console.log(searchInsert([1], 0));
