01c16b4dd10a65725e0676c1b53ed047cfd1596e
gardenia <455419694@qq.com>
2021-06-21 21:04:08 +0800

    leetcode 118

---
+++ 118.pascals-triangle.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=118 lang=javascript
+ *
+ * [118] Pascal's Triangle
+ */
+
+// @lc code=start
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+  let ans = [
+    [1]
+  ]
+  for (let i = 1; i < numRows; i++) {
+    let item = []
+    for (let j = 0; j <= i; j++) {
+      if (j == 0 || j == i) {
+        item[j] = 1
+      } else {
+        item[j] = ans[i - 1][j - 1] + ans[i - 1][j]
+      }
+    }
+    ans[i] = item
+  }
+  return ans
+};
+// @lc code=end
+
+console.log(generate(6));
