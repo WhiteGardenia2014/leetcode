5f70b17665b80a817766bcaac5897953ec2feae0
gardenia <455419694@qq.com>
2021-06-23 16:38:30 +0800

    leetcode 53

---
+++ 53.maximum-subarray.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=53 lang=javascript
+ *
+ * [53] Maximum Subarray
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  let max = -1000000
+  let count = 0
+  for (let i = 0; i < nums.length; i++) {
+    count += nums[i]
+    if (count > max) {
+      max = count
+    }
+    if (count <= 0) {
+      count = 0
+    }
+  }
+  return max
+};
+// @lc code=end
+
+console.log(maxSubArray([-1, 0]));
