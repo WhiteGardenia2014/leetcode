67e7ad7df977a05a1e963396a95537fde71b6402
gardenia <455419694@qq.com>
2021-06-24 12:46:10 +0800

    leetcode 415

--- 415.add-strings.js
+++ 415.add-strings.js
@@ -10,7 +10,8 @@
  * @param {string} num2
  * @return {string}
  */
-var addStrings = function (num1, num2) {
+
+/*var addStrings = function (num1, num2) {
   let n1 = num1.length - 1
   let n2 = num2.length - 1
   let c = 0
@@ -33,7 +34,27 @@ var addStrings = function (num1, num2) {
     ans = "" + c + ans
   }
   return ans
-};
+};*/
+
+var addStrings = function (num1, num2) {
+  let i = num1.length - 1
+  let j = num2.length - 1
+  let ans = ""
+  let carry = 0
+  for (; i >= 0 || j >= 0; i--, j--) {
+    let x = num1[i] | 0
+    let y = num2[j] | 0
+    let s = x + y + carry
+    let z = s % 10
+    carry = (s - z) / 10
+    ans = z + ans
+  }
+  if (carry) {
+    ans = carry + ans
+  }
+  return ans
+}
+
 // @lc code=end
 
 console.log(addStrings("999", "999"))
