c8946a1e9f5e7052b5536a2e4077d3a738fd640c
gardenia <455419694@qq.com>
2021-06-24 17:13:37 +0800

    leetcode 49

--- 49.group-anagrams.js
+++ 49.group-anagrams.js
@@ -10,19 +10,21 @@
  * @return {string[][]}
  */
 
-//使用对象
+//方法一：哈希表中的键为排序后的字符串
+//方法二：哈希表中的键为记录字符串中每个字符出现频数的数组
+
 var groupAnagrams = function (strs) {
   let hash = {}
-  for (let value of strs) {
-    let key = value.split("").sort().join("")
+  for (let value of strs) { //遍历strs数组中的每一个字符串
+    let key = value.split("").sort().join("") //将字符串排序后的新字符串作为键名
     if (key in hash) {
-      hash[key].push(value)
+      hash[key].push(value) //如果存在该键名，就将当前字符串添加到这一类的数组中
     } else {
-      hash[key] = [value]
+      hash[key] = [value] //如果不存在该键名，就开辟一个数组，记录这一类的字符串
     }
   }
   let ans = []
-  for (let key in hash) {
+  for (let key in hash) { //遍历生成好的哈希表，将每一类字符串数组，记录到更大的数组中
     ans.push(hash[key])
   }
   return ans
