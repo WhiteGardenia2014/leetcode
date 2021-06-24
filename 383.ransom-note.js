c0364bf871e073a96676777c90f3ac6b1774a334
gardenia <455419694@qq.com>
2021-06-24 19:33:27 +0800

    leetcode 383

--- 383.ransom-note.js
+++ 383.ransom-note.js
@@ -12,17 +12,17 @@
  */
 var canConstruct = function (ransomNote, magazine) {
   let hash = {}
-  for (let i = 0; i < ransomNote.length; i++) {
+  for (let i = 0; i < ransomNote.length; i++) { //遍历赎金信，记录每个字符出现的频数
     let value = ransomNote[i]
     hash[value] ? hash[value]++ : hash[value] = 1
   }
-  for (let i = 0; i < magazine.length; i++) {
+  for (let i = 0; i < magazine.length; i++) { //遍历杂志，如果有哈希表中的字符，就把对应的频数减 1
     let value = magazine[i]
     if (hash[value]) {
       hash[value]--
     }
   }
-  for (let key in hash) {
+  for (let key in hash) { //如果哈希表中所有的字符的频数都为 0 ， 就说明赎金信可以由杂志拼成
     if (hash[key] > 0) {
       return false
     }
