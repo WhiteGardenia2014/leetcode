fe6fd2384060e93e34a41f8d58e062f45bbff639
gardenia <455419694@qq.com>
2021-06-24 19:21:06 +0800

    leetcode 205

---
+++ 205.isomorphic-strings.js
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=205 lang=javascript
+ *
+ * [205] Isomorphic Strings
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+//双端映射问题
+//需要两张哈希表，记录s中字符和 t中字符的对应关系
+//一旦无法对应，就返回false
+
+var isIsomorphic = function (s, t) {
+  if (s.length != t.length) { //如果两字符串长度不同，直接返回false
+    return false
+  }
+  let sTot = {}
+  let tTos = {}
+  for (let i = 0; i < s.length; i++) {
+    let charS = s[i]
+    let charT = t[i]
+    //如果在 sTot 表中，记录过这个字符 s ，但对应的字符 t 不正确
+    //或者在 tTos 表中，记录过这个字符 t ，但对应的字符 s 不正确
+    //就返回false
+    //否则就将字符 s 和字符 t 的对应关系，记录到两张表中
+    if ((sTot[charS] !== undefined && sTot[charS] !== charT) ||
+      (tTos[charT] !== undefined && tTos[charT] !== charS)) {
+      return false
+    } else {
+      sTot[charS] = charT
+      tTos[charT] = charS
+    }
+  }
+  return true
+};
+// @lc code=end
+
+console.log(isIsomorphic("paper", "title"));
