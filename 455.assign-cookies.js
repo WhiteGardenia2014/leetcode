4f5d71ac170658b3f7902369a2de813a933bbf2b
gardenia <455419694@qq.com>
2021-06-25 19:04:33 +0800

    leetcode 455

--- 455.assign-cookies.js
+++ 455.assign-cookies.js
@@ -10,6 +10,13 @@
  * @param {number[]} s
  * @return {number}
  */
+
+//贪心算法
+//首先对两个数组排序（从小到大）
+//如果当前孩子的饥饿值大于饼干能提供的值，就寻找下一个饼干，直到超过饼干的数量
+//如果当前孩子的饥饿值小于等于饼干能提供的值，则count加一，
+//并将指针指向下一个孩子和下一块饼干
+
 var findContentChildren = function (g, s) {
   g.sort((a, b) => a - b)
   s.sort((a, b) => a - b)
