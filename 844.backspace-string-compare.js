4b383ae736fe40d66b678cf00bd5170455c05b3b
gardenia <455419694@qq.com>
2021-06-25 19:17:30 +0800

    leetcode 844

--- 844.backspace-string-compare.js
+++ 844.backspace-string-compare.js
@@ -10,6 +10,8 @@
  * @param {string} t
  * @return {boolean}
  */
+
+//栈
 var backspaceCompare = function (s, t) {
   return backSpace(s) == backSpace(t)
 };
@@ -17,9 +19,9 @@ var backspaceCompare = function (s, t) {
 function backSpace(s) {
   let array = []
   for (let i = 0; i <= s.length; i++) {
-    if (s[i] != "#") {
+    if (s[i] != "#") { //如果不是 "#" 符，就把字符推入栈
       array.push(s[i])
-    } else {
+    } else { //如果是 "#" 符，就弹出栈顶的字符
       array.pop()
     }
   }
