9a5b6facbae0554d82da0b66735b5beb082adbfb
gardenia <455419694@qq.com>
2021-06-25 19:06:00 +0800

    leetcode 461

--- 461.hamming-distance.js
+++ 461.hamming-distance.js
@@ -10,6 +10,13 @@
  * @param {number} y
  * @return {number}
  */
+
+//位运算、异或运算
+//如果a和b的某一位二进制数不同，那么该位异或的结果就是1
+//因此需要统计 a ^ b 的结果ans中 1 的数量
+
+//也可用Brian Kernighan 算法，n = n & (n - 1) 可以去掉二进制数的最末位的 1
+
 var hammingDistance = function (x, y) {
   let ans = x ^ y
   let num = 0
