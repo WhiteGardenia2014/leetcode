fd5763a3567d37d8a653d78d3f04720fc7e92fb6
gardenia <455419694@qq.com>
2021-06-25 19:30:31 +0800

    leetcode 1475

--- 1475.final-prices-with-a-special-discount-in-a-shop.js
+++ 1475.final-prices-with-a-special-discount-in-a-shop.js
@@ -11,8 +11,8 @@
  */
 var finalPrices = function (prices) {
 
-  for (let i = 0; i < prices.length - 1; i++) {
-    for (let j = i + 1; j < prices.length; j++) {
+  for (let i = 0; i < prices.length - 1; i++) { //从第 0 项开始，遍历价格数组，注意最后一个商品是无法享受到折扣的
+    for (let j = i + 1; j < prices.length; j++) { //对于第 i 个商品，从第 i + 1 项开始，搜索第一个价格小于等于它的价格，即为它能享受的折扣
       if (prices[i] >= prices[j]) {
         prices[i] -= prices[j]
         break
