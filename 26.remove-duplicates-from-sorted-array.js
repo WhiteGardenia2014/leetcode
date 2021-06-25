393293f716a14fd33b3314a2d63a6e0862e72889
gardenia <455419694@qq.com>
2021-06-25 15:04:50 +0800

    leetcode 26

--- 26.remove-duplicates-from-sorted-array.js
+++ 26.remove-duplicates-from-sorted-array.js
@@ -9,9 +9,12 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+//双指针
 var removeDuplicates = function (nums) {
-  let slowIndex = 1
+  let slowIndex = 1 //左右指针从索引 1 开始
   for (let fastIndex = 1; fastIndex < nums.length; fastIndex++) {
+    //如果右指针指向了不同的数字，就赋值给左指针，并使左指针指向下一个位置
     if (nums[fastIndex] != nums[slowIndex - 1]) {
       nums[slowIndex] = nums[fastIndex]
       slowIndex++
