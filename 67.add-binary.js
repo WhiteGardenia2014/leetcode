96d7ceebe77a3a781faf9308a6d0d9fd11bc1a7f
gardenia <455419694@qq.com>
2021-06-25 16:56:15 +0800

    leetcode 67

--- 67.add-binary.js
+++ 67.add-binary.js
@@ -45,16 +45,16 @@ var addBinary = function (a, b) {
   let i = a.length - 1
   let j = b.length - 1
   let ans = ""
-  let carry = 0
-  for (; i >= 0 || j >= 0; i--, j--) {
-    let x = a[i] | 0
+  let carry = 0 //进位初始值设为 0
+  for (; i >= 0 || j >= 0; i--, j--) { //指针从后向前移动，直到移动到更长数字的首位
+    let x = a[i] | 0 //如果某一位数字为undefined，就赋值为0，也就是把较短数字的前面补 0 
     let y = b[j] | 0
     let s = x + y + carry
-    let z = s & 1
-    carry = s >> 1
-    ans = z + ans
+    let z = s & 1 //计算当前位
+    carry = s >> 1 //计算进位
+    ans = z + ans //将当前位 拼到结果的左边
   }
-  if (carry) {
+  if (carry) { //如果最后依然存在进位，就把进位 拼到结果的首位
     ans = carry + ans
   }
   return ans
