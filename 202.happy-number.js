a5397ce175675257d8209906558645f462e3b85d
gardenia <455419694@qq.com>
2021-06-25 18:39:24 +0800

    leetcode 202

--- 202.happy-number.js
+++ 202.happy-number.js
@@ -10,13 +10,17 @@
  * @return {boolean}
  */
 
-//常规算法
+//快乐数只有迭代到 1，或者进入循环两种可能
+
+//可以用数组记录是否进入了循环
+//也可以使用哈希表记录是否进入了循环
+//通过数学方法，可知循环仅有一个
 var isHappy = function (n) {
   let array = []
   let d = 0
   let ans = 0
   while (1) {
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) { //用数组记录每一次的数字，如果当前求出的数字在数组中记录过，就说明进入了循环
       if (array[i] == n) {
         return false
       }
