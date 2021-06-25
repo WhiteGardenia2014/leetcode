88076b753202898d90fcc4eea4799aea47f28e9b
gardenia <455419694@qq.com>
2021-06-25 19:03:02 +0800

    leetcode 415

--- 415.add-strings.js
+++ 415.add-strings.js
@@ -36,21 +36,23 @@
   return ans
 };*/
 
+
+//解法同 67 题
 var addStrings = function (num1, num2) {
   let i = num1.length - 1
   let j = num2.length - 1
   let ans = ""
   let carry = 0
   for (; i >= 0 || j >= 0; i--, j--) {
-    let x = num1[i] | 0
+    let x = num1[i] | 0 //如果某一位数字为undefined，就赋值为0，也就是把较短数字的前面补 0 
     let y = num2[j] | 0
     let s = x + y + carry
-    let z = s % 10
-    carry = (s - z) / 10
-    ans = z + ans
+    let z = s % 10 //计算当前位
+    carry = (s - z) / 10 //计算进位
+    ans = z + ans //将当前位 拼到结果的左边
   }
   if (carry) {
-    ans = carry + ans
+    ans = carry + ans //如果最后依然存在进位，就把进位 拼到结果的首位
   }
   return ans
 }
