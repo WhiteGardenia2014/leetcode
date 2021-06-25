250c5231aa3617d9810cf2d3fe986b22510f2d98
gardenia <455419694@qq.com>
2021-06-25 15:41:13 +0800

    leetcode 50

--- 50.pow-x-n.js
+++ 50.pow-x-n.js
@@ -10,6 +10,8 @@
  * @param {number} n
  * @return {number}
  */
+
+//快速乘方运算
 var myPow = function (x, n) {
   if (n == 0) {
     return 1
@@ -21,6 +23,9 @@ var myPow = function (x, n) {
   }
 };
 
+//递归算法
+//每次计算 y = pow ( x , n / 2 ) ，
+//如果n为偶数，返回 y * y ， 如果n为奇数，返回 y * y * x 
 /*function quickMul(x, n) {
   if (n == 0) {
     return 1
@@ -36,15 +41,19 @@ var myPow = function (x, n) {
   }
 }*/
 
+//循环算法
+//n 的二进制表示中 1 出现的位置，即为需要乘上的因子部分
+//例如 3^27 =  3^1  *  3^2  *  3^8  *  3^16
+// 27(2) = 11011
 
 function quickMul(x, n) {
   let ans = 1
   while (n) {
-    if (n & 1 == 1) {
+    if (n & 1 == 1) { //如果该二进制位为 1，说明这一部分的因子需要乘到 ans中
       ans *= x
     }
-    x *= x
-    n = n >>> 1
+    x *= x //乘方因子每次循环迭代一倍
+    n = n >>> 1 //注意要使用无符号右移
   }
   return ans
 }
