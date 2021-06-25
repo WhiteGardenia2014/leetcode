3abc30901f38eceb1a2a0e11f5253d57e1f9757f
gardenia <455419694@qq.com>
2021-06-25 19:19:27 +0800

    leetcode 922

--- 922.sort-array-by-parity-ii.js
+++ 922.sort-array-by-parity-ii.js
@@ -9,6 +9,14 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+//***** 也可以使用双指针
+//A指针维护偶数索引，B指针维护奇数索引，A，B指针每次 + 2
+//如果A指针内的数字为奇数，就移动B指针，直到找到下一个偶数，
+//并交换A，B指针中的数字，直到遍历完数组
+
+
+//使用两次循环，一次存入所有偶数，一次存入所有奇数
 var sortArrayByParityII = function (nums) {
   let ans = []
   for (let i = 0, j = 0; i < nums.length; i++) {
