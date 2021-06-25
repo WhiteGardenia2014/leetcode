c1477af9184a6c736f0b08cdcdedfc8496302979
gardenia <455419694@qq.com>
2021-06-25 18:58:55 +0800

    leetcode 338

--- 338.counting-bits.js
+++ 338.counting-bits.js
@@ -9,6 +9,12 @@
  * @param {number} n
  * @return {number[]}
  */
+
+//也可以用Brian Kernighan 算法，n = n & (n - 1) 可以去掉二进制数的最末位的 1
+// ans[i] = ans[ i & (i - 1) ] + 1
+
+//如果一个数是奇数，有 ans[i] = ans[i - 1] + 1
+//如果一个数是偶数，有 ans[i] = ans[i >>> 1]
 var countBits = function (n) {
   let ans = [0]
   for (let i = 1; i <= n; i++) {
