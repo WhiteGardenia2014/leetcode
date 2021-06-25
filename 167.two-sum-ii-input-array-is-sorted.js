746d534024e4aeb40495f3df7a83cbe6fe0aea51
gardenia <455419694@qq.com>
2021-06-25 18:10:19 +0800

    leetcode 167

--- 167.two-sum-ii-input-array-is-sorted.js
+++ 167.two-sum-ii-input-array-is-sorted.js
@@ -10,7 +10,7 @@
  * @param {number} target
  * @return {number[]}
  */
-//哈希表
+//哈希表，和第 1 题类似
 /*var twoSum = function (numbers, target) {
   let hash = {}
   for (i = 0; i < numbers.length; i++) {
@@ -22,16 +22,16 @@
   }
 };*/
 
-//双指针
+//缩小搜索范围，双指针
 var twoSum = function (numbers, target) {
   let left = 0
   let right = numbers.length - 1
   while (left < right) {
     if (numbers[left] + numbers[right] == target) {
       return [left + 1, right + 1]
-    } else if (numbers[left] + numbers[right] > target) {
+    } else if (numbers[left] + numbers[right] > target) { //如果当前两数之和大于target，最右边的值一定不是答案，需要舍去
       right--
-    } else if (numbers[left] + numbers[right] < target) {
+    } else if (numbers[left] + numbers[right] < target) { //如果当前两数之和小于target，最左边的值一定不是答案，需要舍去
       left++
     }
   }
