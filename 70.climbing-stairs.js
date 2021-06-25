4d1c3427b2b501e4fe0e5f8d609b012fba04997f
gardenia <455419694@qq.com>
2021-06-25 17:09:41 +0800

    leetcode 70

--- 70.climbing-stairs.js
+++ 70.climbing-stairs.js
@@ -9,6 +9,11 @@
  * @param {number} n
  * @return {number}
  */
+
+//动态规划
+//dp数组的含义为，达到第 i 级台阶的时候，一共有多少种走法
+// dp[i] 的值 取决于 dp[i - 1] 和 dp[i - 2] 内的值
+
 /*var climbStairs = function (n) {
   if (n == 1) {
     return 1
@@ -22,6 +27,8 @@
   return dp[n]
 };*/
 
+//由于 dp[i] 的值 取决于 dp[i - 1] 和 dp[i - 2] 内的值
+//所以不需要维护整个dp数组，只需维护 dp1 和 dp2 变量
 var climbStairs = function (n) {
   if (n == 1) {
     return 1
