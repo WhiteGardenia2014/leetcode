f27e561ab3a2d72f37888820f61e00a0b1f35a47
gardenia <455419694@qq.com>
2021-06-25 18:15:02 +0800

    leetcode 189

--- 189.rotate-array.js
+++ 189.rotate-array.js
@@ -12,6 +12,8 @@
  */
 
 //方法一
+//建立临时数组，新数组各项满足 ans[(i + k) % n] = nums[i]
+//由于是原地算法，需要将新数组 ans 赋值给 nums
 /*var rotate = function (nums, k) {
   let n = nums.length
   let ans = []
@@ -25,6 +27,7 @@
 };*/
 
 //方法二
+//首先翻转数组一次，并将 k 位之前和 k 位之后的两部分数组再次翻转
 var rotate = function (nums, k) {
   k = k % nums.length
   reverseArray(nums, 0, nums.length - 1)
