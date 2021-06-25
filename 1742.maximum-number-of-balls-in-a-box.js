3bd0358a831fba7705f360ae2ba5845957c08ebf
gardenia <455419694@qq.com>
2021-06-25 19:38:58 +0800

    leetcode 1742

--- 1742.maximum-number-of-balls-in-a-box.js
+++ 1742.maximum-number-of-balls-in-a-box.js
@@ -12,12 +12,13 @@
  */
 var countBalls = function (lowLimit, highLimit) {
   let n = Math.ceil(Math.log10(highLimit))
-  let box = Array(9 * n + 2).fill(0)
+  let box = Array(9 * n + 2).fill(0) //如果最大的数字是99，就生成长度为20的数组对应20个盒子
+  //如果只有一个数字，那么也至少需要两个盒子，因为始终存在一个 0 号盒子
 
   for (let i = lowLimit; i <= highLimit; i++) {
     let sum = 0
     let item = i
-    while (item) {
+    while (item) { //计算小球的各位数字和，放入对应的盒子
       sum += item % 10
       item = (item - (item % 10)) / 10
     }
@@ -25,7 +26,7 @@ var countBalls = function (lowLimit, highLimit) {
   }
 
   let max = 0
-  for (let i = 0; i < box.length; i++) {
+  for (let i = 0; i < box.length; i++) { //找到小球数量的最大值
     max = Math.max(max, box[i])
   }
   return max
