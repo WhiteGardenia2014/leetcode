de8975d95393eae0c2e4d606982a1fd6b145999b
gardenia <455419694@qq.com>
2021-06-25 19:26:00 +0800

    leetcode 1351

--- 1351.count-negative-numbers-in-a-sorted-matrix.js
+++ 1351.count-negative-numbers-in-a-sorted-matrix.js
@@ -9,6 +9,12 @@
  * @param {number[][]} grid
  * @return {number}
  */
+
+//***** 这道题也有二分查找和分治算法的解法，
+//逆序遍历
+//利用矩阵横向纵向递减的顺序，从下向上，从右向左，遍历矩阵
+//逐步缩小查找的范围
+
 var countNegatives = function (grid) {
   let m = grid.length
   let n = grid[0].length
@@ -16,7 +22,7 @@ var countNegatives = function (grid) {
   let k = 0
   for (let i = m - 1; i >= 0; i--) {
     for (let j = n - 1; j >= k; j--) {
-      if (grid[i][j] >= 0) {
+      if (grid[i][j] >= 0) { //如果第 i 行的第 j 列的数字不是负数，那么第 i - 1行，只需要逆序遍历到第 j + 1列
         k = j + 1
         break
       } else {
