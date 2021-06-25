dfda0ab38966a5672e3576800bae3933951da99c
gardenia <455419694@qq.com>
2021-06-25 18:12:14 +0800

    leetcode 168

--- 168.excel-sheet-column-title.js
+++ 168.excel-sheet-column-title.js
@@ -9,14 +9,18 @@
  * @param {number} columnNumber
  * @return {string}
  */
+
+//方法一：原理是将10进制转换成26进制，但余数是1-26，而不是0-25，因此每次需要将被除数-1，再取出每一位数
+//方法二：每次遇到整除26的时候，余数不应该是0，应该把余数改成26
+
 var convertToTitle = function (columnNumber) {
   let a = ""
   let d = 0
   do {
-    columnNumber--
-    d = columnNumber % 26
-    a = String.fromCodePoint(d + 65) + a
-    columnNumber = (columnNumber - d) / 26
+    columnNumber-- //将被除数-1
+    d = columnNumber % 26 //取出末尾一位数
+    a = String.fromCodePoint(d + 65) + a //将ASCII值转换为字符
+    columnNumber = (columnNumber - d) / 26 //去掉末尾位，准备计算下一位
   } while (columnNumber)
 
   return a
