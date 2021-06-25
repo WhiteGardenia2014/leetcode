ec05ad8490d60d7501279abae999fade529ab281
gardenia <455419694@qq.com>
2021-06-25 18:55:28 +0800

    leetcode 283

--- 283.move-zeroes.js
+++ 283.move-zeroes.js
@@ -9,15 +9,18 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+
+//双指针
 var moveZeroes = function (nums) {
   let slowIndex = 0
+  //从左到右遍历数组，如果右指针指向的不是 0，就交换左右指针的内容，交换后，左指针指向下一个数
   for (let fastIndex = 0; fastIndex < nums.length; fastIndex++) {
     if (nums[fastIndex] != 0) {
       nums[slowIndex] = nums[fastIndex]
       slowIndex++
     }
   }
-  for (; slowIndex < nums.length; slowIndex++) {
+  for (; slowIndex < nums.length; slowIndex++) { //最后在数组后面补全 0
     nums[slowIndex] = 0
   }
   return nums
