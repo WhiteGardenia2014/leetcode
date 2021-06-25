8893b2ea81b874198a66e5b6b35027ee3164fcfa
gardenia <455419694@qq.com>
2021-06-25 18:06:40 +0800

    leetcode 125

--- 125.valid-palindrome.js
+++ 125.valid-palindrome.js
@@ -10,14 +10,14 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-  s = s.toLowerCase().match(/[a-z0-9]+/g)
-  if (!s) {
+  s = s.toLowerCase().match(/[a-z0-9]+/g) //使用正则表达式匹配，仅保留字符串中的字母
+  if (!s) { //注意，如果字符串没有匹配正则表达式的值，返回的结果是 null，null是不能调用 join() 方法的
     return true
   }
   s = s.join("")
   let leftIndex = 0
   let rightIndex = s.length - 1
-  while (leftIndex < rightIndex) {
+  while (leftIndex < rightIndex) { //双指针从两侧开始，检测对应的字符是否相同，不同则返回false
     if (s[leftIndex] != s[rightIndex]) {
       return false
     }
