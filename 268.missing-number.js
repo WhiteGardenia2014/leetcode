f05a745304b5582118e9b2a747e8c7b249a4dd3b
gardenia <455419694@qq.com>
2021-06-25 18:53:34 +0800

    leetcode 268

--- 268.missing-number.js
+++ 268.missing-number.js
@@ -9,11 +9,14 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+//***** 还有哈希表法、位运算法
+//数学方法
 var missingNumber = function (nums) {
   let l = nums.length
-  let sum = l * (l + 1) / 2
+  let sum = l * (l + 1) / 2 //计算 0 ~ n 的总和
   for (let i = 0; i < l; i++) {
-    sum -= nums[i]
+    sum -= nums[i] //用总和减去数组中出现过的数字，剩下的结果就是丢失的数字
   }
   return sum
 };
