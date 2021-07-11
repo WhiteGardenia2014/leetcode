e25d8866f4c97ca3eb7e7bd9a8d71d75dca7b368
gardenia <455419694@qq.com>
2021-07-11 20:25:12 +0800

    leetcode 160

--- 160.intersection-of-two-linked-lists.js
+++ 160.intersection-of-two-linked-lists.js
@@ -55,4 +55,65 @@ function getLength(head) {
   }
   return length;
 }
+
+
+//方法二：Set 集合
+var getIntersectionNode = function (headA, headB) {
+  let mySet = new Set()
+  while (headA) { //遍历 A 链表，把 A 链表中的节点放入 Set 集合
+    mySet.add(headA)
+    headA = headA.next
+  }
+  while (headB) { //遍历 B 链表，检查 B 链表的节点是否在 Set 集合中存在
+    if (mySet.has(headB)) { //如果存在，说明找到了相交节点，返回这个节点
+      return headB
+    }
+    headB = headB.next
+  }
+  return null //如果没有相交节点，返回 null
+}
+
+
+//方法三：使用 A,B 两个指针，分别沿着 A,B 链表前进，
+//如果 A 指针指向了 A 链表的末尾，就从 B 链表的头部重新开始
+//如果 B 指针指向了 B 链表的末尾，就从 A 链表的头部重新开始
+//如果 A，B 存在相交节点，那么两个指针在第二次前进时一定会找到相交节点
+//如果 A，B 不存在相交节点，那么两个指针一定会最终都指向 null
+var getIntersectionNode = function (headA, headB) {
+  if (headA === null || headB === null) {
+    return null
+  }
+  let nodeA = headA
+  let nodeB = headB
+  while (nodeA || nodeB) {
+    if (nodeA === nodeB) {
+      return nodeA
+    }
+    nodeA = nodeA === null ? headB : nodeA.next //如果 A 指针指向了 A 链表的末尾，就从 B 链表的头部重新开始
+    nodeB = nodeB === null ? headA : nodeB.next //如果 B 指针指向了 B 链表的末尾，就从 A 链表的头部重新开始
+  }
+  return null
+}
+
+
+//方法四：方法三的递归写法
+var getIntersectionNode = function (headA, headB) {
+  if (headA === null || headB === null) {
+    return null
+  }
+  return findNode(headA, headB)
+
+  function findNode(nodeA, nodeB) {
+    if (nodeA === nodeB) {
+      return nodeA
+    }
+    if (nodeA === null) {
+      return findNode(headB, nodeB.next)
+    }
+    if (nodeB === null) {
+      return findNode(nodeA.next, headA)
+    }
+    return findNode(nodeA.next, nodeB.next)
+  }
+}
 // @lc code=end
