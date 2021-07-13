0629f834b31c4080f40d2356ac9b7fc9be9dc864
gardenia <455419694@qq.com>
2021-07-13 20:20:41 +0800

    leetcode 107

---
+++ 107.binary-tree-level-order-traversal-ii.js
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=107 lang=javascript
+ *
+ * [107] Binary Tree Level Order Traversal II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function (root) {
+  if (!root) {
+    return []
+  }
+  let res = levelOrder(root)
+  return res.reverse()
+};
+
+//层序遍历
+function levelOrder(root) {
+  if (!root) {
+    return []
+  }
+  let res = []
+  let queue = [] //记录每一层节点值的队列
+  queue.push(root)
+  while (queue.length) {
+    //记录当前层的节点数
+    let length = queue.length
+    let curLevel = [] //记录当前层节点值的数组
+    for (let i = 0; i < length; i++) {
+      let node = queue.shift() //每次从 queue 中取出一个节点
+      curLevel.push(node.val)
+      //把当前层的子节点按顺序放入 queue 中
+      node.left && queue.push(node.left)
+      node.right && queue.push(node.right)
+    }
+    res.push(curLevel) //把当前层节点值的数组记录到结果中
+  }
+  return res
+};
+// @lc code=end
