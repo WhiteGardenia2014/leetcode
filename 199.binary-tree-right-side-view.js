ee06d84bd19e5c17700e4d2ef62d96c409c14b85
gardenia <455419694@qq.com>
2021-07-13 20:33:22 +0800

    leetcode 199

---
+++ 199.binary-tree-right-side-view.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=199 lang=javascript
+ *
+ * [199] Binary Tree Right Side View
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function (root) {
+  if (!root) {
+    return []
+  }
+  let res = []
+  let queue = [] //记录每一层节点值的队列
+  queue.push(root)
+  while (queue.length) {
+    //记录当前层的节点数
+    let length = queue.length
+    for (let i = 0; i < length; i++) {
+      let node = queue.shift() //每次从 queue 中取出一个节点
+      if (i == length - 1) { //如果是这一层的最后一个节点，就把它的值记录到结果中
+        res.push(node.val)
+      }
+      //把当前层的子节点按顺序放入 queue 中
+      node.left && queue.push(node.left)
+      node.right && queue.push(node.right)
+    }
+  }
+  return res
+};
+// @lc code=end
