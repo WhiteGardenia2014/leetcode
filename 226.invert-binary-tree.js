9c3c544510f7b47412c09f9f27da100daf9d88f9
gardenia <455419694@qq.com>
2021-07-14 12:42:34 +0800

    leetcode 226

---
+++ 226.invert-binary-tree.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=226 lang=javascript
+ *
+ * [226] Invert Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  if (root) {
+    let invertLeft = invertTree(root.left)
+    let invertRight = invertTree(root.right)
+    root.left = invertRight
+    root.right = invertLeft
+  }
+  return root
+};
+// @lc code=end
