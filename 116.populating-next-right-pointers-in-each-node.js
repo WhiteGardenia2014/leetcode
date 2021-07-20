520009256781f179b8d857f9b102a2dbdf7bf835
gardenia <455419694@qq.com>
2021-07-20 10:06:01 +0800

    leetcode 116

--- 116.populating-next-right-pointers-in-each-node.js
+++ 116.populating-next-right-pointers-in-each-node.js
@@ -74,6 +74,35 @@ var connect = function (root) {
 }
 
 
+//层序遍历解法，使用已经建立的 next 指针
+//使用哑节点，通用性更好，与 117 题相同
+var connect = function (root) {
+  if (!root) { //如果没有节点，返回 null
+    return null
+  }
+  let cur = root
+  while (cur) { //外层循环遍历每一层，遍历到第 i 层节点时，为第 i+1 层节点建立连接
+    //为方便处理，在第 i+1 层节点前添加一个哑节点
+    let dummy = new Node(0)
+    let pre = dummy
+    while (cur) { //内层循环遍历当前层的节点，注意该层的 next 指针已经由上一层建立好
+      if (cur.left) { //如果左节点存在，就连接左节点，pre 指针移动至待连接节点
+        pre.next = cur.left
+        pre = pre.next
+      }
+      if (cur.right) { //如果右节点存在，就继续连接右节点，pre 指针再次移动
+        pre.next = cur.right
+        pre = pre.next
+      }
+      cur = cur.next // cur 指针改为右侧的节点，遍历当前层
+    }
+    cur = dummy.next // cur 指针指向下一层的首节点，循环处理下一层
+  }
+  return root
+}
+
+
+
 //自顶向下，递归解法，空间复杂度 O(1)
 var connect = function (root) {
   if (!root) { //如果没有节点，返回 null
