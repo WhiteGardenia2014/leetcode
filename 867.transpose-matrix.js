59ee05e3133a5238d3fd083c43887b41448ed25d
gardenia <455419694@qq.com>
2021-07-21 21:20:37 +0800

    leetcode 867

--- 867.transpose-matrix.js
+++ 867.transpose-matrix.js
@@ -17,4 +17,23 @@ var transpose = function (matrix) {
     }, [])
   })
 };
+
+
+var transpose = function (matrix) {
+  return matrix[0].map((_, index) => matrix.map(row => row[index]))
+}
+
+
+var transpose = function (matrix) {
+  let m = matrix.length
+  let n = matrix[0].length
+  let ans = Array(n).fill(0).map(it => Array(m).fill(0))
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      ans[j][i] = matrix[i][j]
+    }
+  }
+  return ans
+}
+
 // @lc code=end
