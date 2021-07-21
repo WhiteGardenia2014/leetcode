90718843f88e3f98941d11f727a428ca4186f3f7
gardenia <455419694@qq.com>
2021-07-21 19:39:15 +0800

    leetcode 606

--- 606.construct-string-from-binary-tree.js
+++ 606.construct-string-from-binary-tree.js
@@ -26,7 +26,7 @@ var tree2str = function (root) {
     if (!root.right) {
       return root.val + "(" + tree2str(root.left) + ")"
     }
-    //如果节点没有左子树，不能省略左子树的括号
+    //如果节点没有左子树，不能省略左子树的括号，这种情况也可以和最终情况合并
     if (!root.left) {
       return root.val + "()(" + tree2str(root.right) + ")"
     }
